b58f0163843a5effe01986fc918f4eb7293401f0
PPJP246 <rockzx007@gmail.com>
2026-06-05 10:48:17 +0700

    fix: เพิ่ม consolo.log

--- server/repositories/sync.repository.js
+++ server/repositories/sync.repository.js
@@ -2,19 +2,28 @@ import syncDb from "../config/sync.db.js";
 
 export async function saveHistory(ans) {
 
-  if (!ans.length) return;
+  if (!Array.isArray(ans) || ans.length === 0) {
+    console.log("No AN to save");
+    return;
+  }
 
   const values = ans.map(an => [
     an,
     new Date()
   ]);
 
-  await db.query(`
-      INSERT INTO sync_log
-      (
-        an,
-        sent_at
-      )
-      VALUES ?
+  console.log("Inserting to sync_log:", values);
+  console.log("ANS:", ans);
+  console.log("SUCCESS ANS:", successAns);
+  console.log("VALUES:", values);
+
+  await syncDb.query(`
+    INSERT INTO sync_log
+    (
+      an,
+      sent_at
+    )
+    VALUES ?
   `, [values]);
+
 }
