6fbf1fffb49182248343e73f9ffd1677b42efac0
PPJP246 <rockzx007@gmail.com>
2026-06-05 11:01:28 +0700

    fix : เอา join ออก

--- server/repositories/patient.repository.js
+++ server/repositories/patient.repository.js
@@ -1,58 +1,25 @@
-import hisDb from "../config/hisdb.js";
-
 export async function getPatients() {
 
   const [rows] = await hisDb.query(`
     SELECT
         an.hn,
         p.cid,
-
         an.an,
-
-        DATE_FORMAT(
-            an.regdate,
-            '%Y-%m-%d'
-        ) AS reg_date,
-
-        an.dchdate,
-
-        CONCAT(
-            p.pname,
-            p.fname,
-            ' ',
-            p.lname
-        ) AS full_name,
-
+        DATE_FORMAT(an.regdate, '%Y-%m-%d') AS reg_date,
+        CONCAT(p.pname, p.fname, ' ', p.lname) AS full_name,
         TIMESTAMPDIFF(
             YEAR,
-            STR_TO_DATE(
-                p.birthday,
-                '%d/%m/%Y'
-            ),
+            STR_TO_DATE(p.birthday, '%d/%m/%Y'),
             CURDATE()
-        ) AS age_years,
-
-        r.hospcode
-
+        ) AS age_years
     FROM referout r
-
-    LEFT JOIN an_stat an
-        ON an.an = r.vn
-
-    LEFT JOIN patient p
-        ON p.hn = r.hn
-
-    LEFT JOIN test_api_post.sync_log s
-        ON s.an = an.an
-
+    LEFT JOIN an_stat an ON an.an = r.vn
+    LEFT JOIN patient p ON p.hn = r.hn
     WHERE an.ward = '04'
       AND r.hospcode = '10690'
       AND an.dchdate IS NOT NULL
-      AND s.an IS NULL
-
     LIMIT 1
   `);
 
   return rows;
-
 }
